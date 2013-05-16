eb7fd1bb2ef29b35e70927d7799516db18648b16
Tom <tommy.joulin@iut-rodez.fr>
2013-05-16 18:53:25 +0200

    Implémentation de Batracien, CraGre, Etang

--- src/craEtGre/Plateau/Batracien.java
+++ src/craEtGre/Plateau/Batracien.java
@@ -16,6 +16,11 @@ public class Batracien {
     private CraGre type;
     
     /**
+     * Etang dans lequel existe le batracien
+     */
+    private Etang etang;
+    
+    /**
      * Couloir du batratien (il ne peut pas en changer)
      */
     private int couloir;
@@ -23,56 +28,157 @@ public class Batracien {
     /**
      * Nénuphar sur lequel se trouve le batracien
      */
-    private int nenuphar;
-    
+    private int feuille;
+        
     /**
      * Crée un batracien d'un type donné 
      * 
      * @param type Type de batracien
      */
-    public Batracien(CraGre type) {
+    public Batracien(Etang etang, CraGre type) {
         
+    	this.etang = etang;
         this.type = type;
         
     }
     
     /**
-     * Methode permettent de dÃ©placer le batracien
-     * (saut ou avancer suivant la situation)
+     * Déplace le batracien
+     * 
+     * @return Vrai si le batracien a pu être déplacé, false si aucun mouvement
+     *         n'était possible.
      */
-    public void deplacer(){
+    public boolean deplacer() {
     
+    	if (peutAvancer()) {
+    		
+    		etang.setBatracien(feuille + type.sens(), this);
+    		return true;
+    		
+    	}
+    	
+    	if (peutSauter()) {
+    		
+    		etang.setBatracien(feuille + (type.sens() * 2), this);
+    		return true;
+    		
+    	}
+    	
+    	return false;
+    	
     }
     
     /**
-     * Teste la possibilitÃ© de dÃ©placement 
+     * Teste la possibilité de déplacement
+     * 
+     * @return Vrai si un déplacement est possible
      */
     public boolean peutSeDeplacer() {
+        
+    	return peutAvancer() || peutSauter();
+    	
+    }
+    
+    /**
+     * Teste la possibilité d'avancer à la feuille suivante
+     * 
+     * @return Vrai si le batracien peut avancer
+     */
+    public boolean peutAvancer() {
+    	
+    	return (suivant() == null);
+    	
+    }
+    
+    /**
+     * Teste la possibilité de sauter une feuille
+     * 
+     * @return Vrai si le batracien peut sauter
+     */
+    public boolean peutSauter() {
+    	
+    	Batracien suivant;
+    	
+    	suivant = suivant();
+    	
+    	if (suivant != null && suivant.type != type) {
+    		
+    		return (suivant.precedent() == null);
+    		
+    	}
+    	
+    	return false;
+    	
+    }
+    
+    /**
+     * Retourne une référence vers le batracien situé sur la feuille suivante
+     * (dans le sens de déplacement du batracien)
+     * 
+     * @return Référence vers un batracien, ou null si aucun batracien n'a pu
+     *         être trouvé.
+     */
+    public Batracien suivant() {
+    	
+    	return etang.getBatracien(couloir, feuille + type.sens());
+    	
+    }
+    
+    /**
+     * Retourne une référence vers le batracien situé sur la feuille précédente
+     * (dans le sens de déplacement du batracien)
+     * 
+     * @return Référence vers un batracien, ou null si aucun batracien n'a pu
+     *         être trouvé.
+     */
+    public Batracien precedent() {
+    	
+    	return etang.getBatracien(couloir, feuille - type.sens());
+    	
+    }
+
+    /**
+     * Retourne le type du batracien
+     * 
+     * @return Type de batracien
+     */
+    public CraGre getType() {
+    	
+        return type;
         
     }
 
     /**
-     * Retourne le type 
-     * @return the type
-     */
-    public CraGre getType() {
-        return type;
-    }
-
-    /**
-     * retourne le couloir 
-     * @return the couloir
+     * Retourne le couloir dans lequel se trouve le batracien
+     * 
+     * @return Couloir dans lequel se trouve le batracien
      */
     public int getCouloir() {
+    	
         return couloir;
+        
     }
 
     /**
-     * retourne le nenuphar
-     * @return the nenuphar
+     * Retourne le nénuphar sur lequel se trouve le batracien
+     * 
+     * @return Nénuphar sur lequel se trouve le batracien
      */
     public int getNenuphar() {
-        return nenuphar;
+    	
+        return feuille;
+        
+    }
+    
+    /**
+     * Change la feuille sur laquelle se trouve le batracien
+     * 
+     * @param feuille Nouvelle position
+     */
+    public void setNenuphar(int feuille) {
+    	
+    	this.feuille = feuille;
+    	
     }
     
 }
--- src/craEtGre/Plateau/Etang.java
+++ src/craEtGre/Plateau/Etang.java
@@ -1,20 +1,106 @@
 /**
- * Etang.java                                      9 mai 2013
- * Ben
+ * Etang.java                                                           16/05/13
  */
 package craEtGre.Plateau;
 
 /**
- * @author Bercy
- * Classe instanciable permettant de représenter Le plateau de Jeu.
- * Elle Permet aussi de repérer la position des batraciens.
+ * Représentation de la zone de jeu
+ * 
+ * @author INFO1
  */
 public class Etang {
 
+	/**
+	 * Nombre maximum de couloirs
+	 */
+	private final int HAUTEUR_MAX = 10;
+	
+	/**
+	 * Nombre maximum de feuilles par couloir
+	 */
+	private final int LARGEUR_MAX = 20;
+	
     /**
-     * Représentation de l'étang comme étant 
-     * un tableau de référances vers des batratiens
+     * Tableau bidimensionnel représentant la position des batraciens
      */
     private Batracien[][] etang;
+    
+    /**
+     * Nombre de couloirs
+     */
+    private int hauteur;
+    
+    /**
+     * Nombre de feuilles par couloir
+     */
+    private int largeur;
+    
+    /**
+     * Construit un étant à partir des dimensions données
+     * 
+     * @param hauteur Nombre de couloirs
+     * @param largeur Feuilles par couloir
+     */
+    public Etang(int hauteur, int largeur) {
+    	
+    	// Si la hauteur ou la largeur est invalide, les valeurs maximums sont
+    	// utilisées par défaut
+    	
+    	if (hauteur < 1 || hauteur > HAUTEUR_MAX) {
+    		
+    		hauteur = HAUTEUR_MAX;
+    		
+    	}
+    	
+    	if (largeur < 1 || largeur > LARGEUR_MAX) {
+    		
+    		largeur = LARGEUR_MAX;
+    		
+    	}
+    	
+    	this.hauteur = hauteur;
+    	this.largeur = largeur;
+    	
+    	etang = new Batracien[hauteur][largeur];
+    	
+    }
+    
+    /**
+     * Retourne une référence vers un batracien à une position donnée
+     * 
+     * @param couloir Couloir à éxaminer
+     * @param feuille Feuille à éxaminer
+     * @return Batracien situé sur {@code feuille}, dans {@code couloir}; ou
+     *         null si aucun batracien n'a pu être trouvé.
+     */
+    public Batracien getBatracien(int couloir, int feuille) {
+    	
+    	if ((couloir >= 0 && couloir < hauteur)
+    			&& (feuille >= 0 && feuille < largeur)) {
+    		
+    		return etang[couloir][feuille];
+    		
+    	}
+    	
+    	return null;
+    	
+    }
+    
+    /**
+     * Positionne un batracien à une position donnée
+     * 
+     * @param feuille Nouvelle position
+     * @param Batracien à placer
+     */
+    void setBatracien(int feuille, Batracien batracien) {
+    	
+    	if (feuille >= 0 && feuille < largeur) {
+    		
+	    	etang[batracien.getCouloir()][feuille] = batracien;
+	    	batracien.setNenuphar(feuille);
+	    	
+    	}
+    	
+    }
     
 }
